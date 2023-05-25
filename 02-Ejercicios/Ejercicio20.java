0923c0d02234e04b8b18fb7581554ffcd27d5984
CristianArce2002 <cris.arce1864@gmail.com>
2023-05-24 21:22:39 -0500

    ejercicio 20 corregido

--- 02-Ejercicios/Ejercicio20.java
+++ 02-Ejercicios/Ejercicio20.java
@@ -6,31 +6,19 @@ public class Ejercicio20 {
 
         Scanner entrada = new Scanner(System.in);
 
-        String numero = "";
+        System.out.print("Ingrese un número entero (5 digitos maximo):");
 
-        int suma = 0;
+        int numero = entrada.nextInt();
 
-        char digito = 0;
+        int digito1 = numero / 10000;
+        int digito2 = (numero % 10000) / 1000;
+        int digito3 = (numero % 1000) / 100;
+        int digito4 = (numero % 100) / 10;
+        int digito5 = (numero % 10) / 1;
 
-        int valor_digito = 0;
+        int suma = digito1 + digito2 + digito3 + digito4 + digito5;
 
-        System.out.print("Ingresa un número entero:");
-
-        numero = entrada.nextLine();
-
-        int cantidad = numero.length();
-
-        for (int i = 0; i < cantidad; i++) {
-
-            digito = numero.charAt(i);
-
-            valor_digito = Character.getNumericValue(digito);
-
-            suma = suma + valor_digito;
-
-        }
-
-        System.out.println("La suma de los dígitos es: " + suma);
+        System.out.println("la suma de los 5 digitos es: " + suma);
         
     }
 
